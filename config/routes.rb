c68746c71708cd27309b6296c6db792c31a3b0cd
Tanisha <tanishahampden@gmail.com>
2017-10-08 11:07:49 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'dates/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/dates', to: 'dates#index', as: 'dates'
+  resources :dates
 end
